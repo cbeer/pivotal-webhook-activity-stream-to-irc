6076b0491a16bc44369e11a7b01b80593b5cc233
Edwin Shin <edwin.shin@yourmediashelf.com>
2013-02-20 11:39:30 +0800

    Switch nick back to ff-pivotal

--- bot.rb
+++ bot.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 
 bot = Cinch::Bot.new do
   configure do |c|
-  	c.nick = "ff-pivotal2"
+  	c.nick = "ff-pivotal"
     c.server = "irc.freenode.org"
     c.channels = ["#duraspace-ff"]
   end
